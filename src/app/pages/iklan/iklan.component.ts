6b38478db4552f0ef8616296a071347355a8f125
Wasyraaf <asyraafwan2003@gmail.com>
2025-03-25 15:34:32 +0800

    tambah part iklan

---
+++ src/app/pages/iklan/iklan.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-iklan',
+  imports: [],
+  templateUrl: './iklan.component.html',
+  styleUrl: './iklan.component.css'
+})
+export class IklanComponent {
+
+}
