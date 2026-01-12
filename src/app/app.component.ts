493f5545d370355627eb57152e11eed6c85c042f
Wasyraaf <asyraafwan2003@gmail.com>
2026-01-12 15:27:00 +0800

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomepageComponent } from "./pages/homepage/homepage.component";
 import { SolatResponse, SolatService, Zone } from './solat.service';
-import { DOCUMENT } from '@angular/common';
+import { DOCUMENT, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomepageComponent], // Remove HttpClientModule from here
+  imports: [RouterOutlet, DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
